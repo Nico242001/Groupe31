fb2d3e161a362f61d74df2745fda098a394b7ebc
Sergio12321 <71737427+Sergio12321@users.noreply.github.com>
2021-10-24 15:24:15 -0500

    Update CreeCompte.java

--- Projet Android/app/src/main/java/com/example/servicenovigrad/CreeCompte.java
+++ Projet Android/app/src/main/java/com/example/servicenovigrad/CreeCompte.java
@@ -11,4 +11,69 @@ public class CreeCompte extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cree_compte);
     }
+    
+    String databaseprenom;
+	String databasemotdepasse;
+	String prenom;
+	String nom;
+	String datenai;
+	String adresse;
+	String type;
+	String motdepasse;
+	
+	public CreeCompte (String p, String n, String d, String a, String t) {
+		prenom = p;
+		nom = n;
+		datenai = d;
+		adresse = a;
+		type = t;
+		
+	}
+	public void setPrenom(String p) {
+		prenom = p;
+	}
+	
+	public String getPrenom () {
+		return prenom;
+	}
+	
+	public void setNom(String n) {
+		nom = n;
+	}
+	
+	public String getNom () {
+		return nom;
+	}
+	
+	public void setDate(String d) {
+		datenai = d;
+	}
+	
+	public String getDate () {
+		return datenai;
+	}
+	
+	public void setAdresse(String a) {
+		adresse = a;
+	}
+	
+	public String getAdresse () {
+		return adresse;
+	}
+	
+	public void setType(String t) {
+		type = t;
+	}
+	
+	public String getType () {
+		return type;
+	}
+	
+	public boolean verifier() {
+		if (prenom == databaseprenom && motdepasse == databasemotdepasse) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
